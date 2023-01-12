12886cbceb3a2a6e04f36e76b6beb9acfe339ed1
Svetlana Vasilevich <117487565+SvetlanaVet@users.noreply.github.com>
2023-01-12 16:01:42 +0000

    add unsubscribe from all

--- cypress/integration/finish_mama_project/social/social.spec.js
+++ cypress/integration/finish_mama_project/social/social.spec.js
@@ -8,7 +8,7 @@ function selectFirstArticle() {
     .find('article-preview')
     .should('have.length', 10)
     .eq(0)
-    .as('randomArticle');
+    .as('firstArticle');
 }
 function waitForLoadArticleList() {
   cy.get('@articleList')
@@ -21,7 +21,32 @@ function openGlobalFeed() {
 }
 
 function checkActiveYourFeed() {
-  cy.get('.feed-toggle a.nav-link.active[ng-class*=feed]').should('be.exist');
+  cy.get('@yourFeed').should('have.class', 'active');
+}
+
+function unsubscribeFromUser() {
+  checkActiveYourFeed();
+  selectFirstArticle();
+  cy.get('@firstArticle').find('.author').click();
+  cy.get('follow-btn button')
+    .should('contain', 'Unfollow')
+    .click()
+    .as('UnfollowButton');
+  // TODO:  improve css
+  cy.get('@UnfollowButton').should('contain', 'Follow');
+
+}
+
+function unsubscribeFromAll() {
+  waitForLoadArticleList();
+  cy.get('@articleList').find('article-preview')
+    .then(articles => {
+      if (articles.length > 0) {
+        cy.wrap(articles).eq(0).click();
+        unsubscribeFromUser();
+        unsubscribeFromAll();
+      }
+    });
 }
 
 describe('Social', () => {
@@ -30,26 +55,29 @@ describe('Social', () => {
     cy.get('.navbar').should('be.visible').as('appHeader');
     login();
     cy.get('article-list').as('articleList');
+    cy.get('@appHeader').find('[show-authed=true] [ui-sref="app.home"]').as('buttonHome');
+    cy.get('.feed-toggle a.nav-link[ng-class*=feed]').as('yourFeed');
     cy.location('hash').should('eq', '#/');
   });
 
   it('should do subscribe to user', () => {
 
-    checkActiveYourFeed();
+    unsubscribeFromAll();
+
+    cy.get('@buttonHome').click();
     
     openGlobalFeed();
-
     selectFirstArticle();
     
-    cy.get('@randomArticle').find('.author').as('describedAuthor').click();
+    cy.get('@firstArticle').find('.author').as('describedAuthor').click();
     cy.get('@describedAuthor')
       .invoke('text')
       .invoke('trim')
       .as('subscribedAuthorName');
 
-    cy.get('@subscribedAuthorName').then((element) => {
-      cy.log(element);
-      console.log(element);
+    cy.get('@subscribedAuthorName').then((authorName) => {
+      cy.log(authorName);
+      console.log(authorName);
 
       cy.get('follow-btn button')
         .should('contain', 'Follow')
@@ -62,7 +90,7 @@ describe('Social', () => {
         .find('[show-authed=true] [ui-sref="app.home"]')
         .click();
 
-      cy.get('.home-page').should('contain', element);
+      cy.get('.home-page').should('contain', authorName);
 
     });
   });
--- test_cases/social/subscribe_to_user.md
+++ test_cases/social/subscribe_to_user.md
@@ -8,14 +8,14 @@ Subscribe to user test case
 2. Repeat 2-9 from [login user](/test_cases/login_user.md)
 3. Url should be `/#/` — main page
 
-## Delete all subscriptions
+## Unsubscribe from all
 
-4.  In feed toggle menu should be activ **Your Feed**
-5.  Your Feed contains text "No articles are here... yet"?
+4. In feed toggle menu should be active **Your Feed**
+5. Your Feed contains text "No articles are here... yet"?
     * No, continue.
     * Yes, go to 11.
 6. Select first article    
-7.  Click button **Unfollow**
+7. Click button **Unfollow**
 8.  Confirm that the button text changes to **Follow**
 9.  Click **Home**
 10. Back to step 4
@@ -28,13 +28,13 @@ Subscribe to user test case
 
 ## Subscribe to user by following him
 
-14. Click buttun **Follow**
+14. Click button **Follow**
 15. Confirm that the button text changes to **Unfollow**
 
 ## Check user in your subscription list
 
 16. Click **Home**
-17. In feed toggle menu should be activ **Your Feed**
+17. In feed toggle menu should be active **Your Feed**
 18. Check `{author}` should be visible
 
 ## Where:
